d2c2f1752e56049445afa92bd594a45225e57d1c
Fredrik Rødsand <fredrikrod@elkjop.no>
2022-11-08 09:30:04 +0100

    Created Message and MessageDao classes. send and retrieve methods in MessageDao

---
+++ src/main/java/no/kristiania/chats/Message.java
@@ -0,0 +1,41 @@
+package no.kristiania.chats;
+
+public class Message {
+
+    private int id;
+
+    private String subject;
+    private String messageBody;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessageBody() {
+        return messageBody;
+    }
+
+    public void setMessageBody(String messageBody) {
+        this.messageBody = messageBody;
+    }
+
+    @Override
+    public String toString(){
+        return "Message {\n" +
+                "Subject: " + subject + "\n" +
+                "message body: " + messageBody + "\n" +
+                "}";
+    }
+}
--- src/main/java/no/kristiania/chats/MessageDao.java
+++ src/main/java/no/kristiania/chats/MessageDao.java
@@ -1,4 +1,63 @@
 package no.kristiania.chats;
 
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 public class MessageDao {
+
+    private final DataSource dataSource;
+
+    public MessageDao(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    public void sendMessage(Message message) throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            var sql = "insert into messages (subject, message_body) values (?, ?)";
+            try (var query = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+                query.setString(1, message.getSubject());
+                query.setString(2, message.getMessageBody());
+                query.executeUpdate();
+
+                try (var rs = query.getGeneratedKeys()) {
+                    rs.next();
+                    message.setId(rs.getInt(1));
+                }
+            }
+        }
+    }
+
+
+    public Message retrieveMessage(int id) throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            var sql = "";
+            try (var query = connection.prepareStatement(sql)) {
+                query.setInt(1, id);
+                try (var rs = query.executeQuery()) {
+                    rs.next();
+                    return createMessage(rs);
+                }
+            }
+        }
+    }
+
+    private static Message createMessage(ResultSet rs) throws SQLException {
+        var message = new Message();
+        message.setId(rs.getInt("id"));
+        message.setSubject(rs.getString("subject"));
+        message.setMessageBody(rs.getString("message_body"));
+        return message;
+    }
+
+    public List<Message> getMessageBySubject(){
+        return null;
+    }
+
+    public List<Message> showAllMessages(){
+        return null;
+    }
+
 }
