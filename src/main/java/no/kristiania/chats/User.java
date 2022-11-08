397f68e1f61a3de92601301a360ea0c68010de6b
Fredrik Rødsand <fredrikrod@elkjop.no>
2022-11-08 09:54:24 +0100

    User class

---
+++ src/main/java/no/kristiania/chats/User.java
@@ -0,0 +1,23 @@
+package no.kristiania.chats;
+
+public class User {
+
+    private int id;
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
