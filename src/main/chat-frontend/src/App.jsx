7d840bbbbaff0ab09c309bfe88cd0c968cbe6c7b
Fredrik Rødsand <fredrikrod@elkjop.no>
2022-11-12 12:06:39 +0100

    frontend now shows messages

--- src/main/chat-frontend/src/App.jsx
+++ src/main/chat-frontend/src/App.jsx
@@ -1,21 +1,96 @@
 import reactLogo from './assets/react.svg'
+import * as React from "react";
 import './App.css'
+import {useEffect, useState} from "react";
+
+
+
+function MessageList(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [users, setUsers] = useState([]);
+
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("/api/chat");
+            if (response.ok){
+                setUsers(await response.json());
+                setIsLoading(false);
+            }
+        })()
+    }, []);
+
+    if (isLoading){
+        return <div>Loading...</div>
+    }
+
+    function handleOnClick(){
+
+    }
+
+    return users.map(u => <button onClick={handleOnClick}>u.name</button>)
+
+}
+
+function SendMessage(){
+
+    const [subject, setSubject] = useState("");
+    const [messageBody, setMessageBody] = useState("");
+
+    async function handleOnSubmit(e){
+        e.preventDefault();
+        await fetch("/api/chat", {
+            method: "post",
+            body: JSON.stringify({subject, messageBody}),
+            headers: {
+                "Content-Type": "application/json"
+            },
+        });
+    }
+
+    return(
+        <>
+            <form onSubmit={handleOnSubmit}>
+                <div>
+                    <label>
+                        Subject: {" "}
+                        <input
+                            type={"text"}
+                            value={subject}
+                            onChange={(e) => setSubject(e.target.value)}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Body: {" "}
+                        <input
+                            type={"text"}
+                            value={messageBody}
+                            onChange={(e) => setMessageBody(e.target.value)}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <button>Submit</button>
+                </div>
+            </form>
+        </>
+    )
+}
+
 
 function App() {
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Chat</h1>
-
-    </div>
+    <>
+        <div className={"app"}>
+            <h1>
+                Messages
+            </h1>
+            <MessageList/>
+            <SendMessage/>
+        </div>
+    </>
   )
 }
 
